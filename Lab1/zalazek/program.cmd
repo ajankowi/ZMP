81765fe87517cf1ef69d3a48f4528d04761ecd5b
ajankowi <252919@student.pwr.edu.pl>
2022-11-27 15:39:43 +0100

    Dodanie obiektów

--- Lab1/zalazek/program.cmd
+++ Lab1/zalazek/program.cmd
@@ -1,24 +1,37 @@
-#define MACRO 60
-#define MACRO2 70
-#define MACRO3 80
+#define NIGHTY 90
+#define FOURTYFIVE 45
 
- 
 
-Begin_Parallel_Actions
-Rotate Podstawa 5 Z 180  
-Rotate Podstawa.Ramie1 2 Y 20  
-End_Parallel_Actions
+ /*
 
 Begin_Parallel_Actions
-Rotate Podstawa.Ramie1.Ramie2 2 Z 45  
-Rotate Podstawa.Ramie1 2 Y 20  
+Rotate Podstawa 5 Z NIGHTY  
 End_Parallel_Actions
 
-Begin_Parallel_Actions
-Rotate Kadlub.przod.piasta.prop 50 Y 2000 
+Begin_Parallel_Actions 
+Rotate Podstawa.Ramie1.Ramie2 1 Y FOURTYFIVE
+Rotate Podstawa.Ramie1 1 Y FOURTYFIVE
 End_Parallel_Actions
 
-Begin_Parallel_Actions
-Rotate Kadlub.przod.piasta.prop 120 Y 6000
-Move Kadlub 0.2  10
+Begin_Parallel_Actions 
+Rotate Podstawa 4 Z 180  
 End_Parallel_Actions
+
+Begin_Parallel_Actions 
+Rotate Podstawa.Ramie1.Ramie2 1 Y FOURTYFIVE
+Rotate Podstawa.Ramie1 1 Y FOURTYFIVE
+End_Parallel_Actions
+
+
+
+*/
+
+
+
+Begin_Parallel_Actions
+Move Car 0.2  25
+Rotate Car.PP 5 Y 1000 
+Rotate Car.LP 5 Y 1000 
+Rotate Car.PT 5 Y 1000 
+Rotate Car.LT 5 Y 1000 
+End_Parallel_Actions  
