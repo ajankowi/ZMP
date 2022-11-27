92658f4778d634bf8ec7ea279a764f9fbe2ff4bc
ajankowi <252919@student.pwr.edu.pl>
2022-11-27 15:42:14 +0100

    Nowe wartości

--- Lab1/zalazek/program.cmd
+++ Lab1/zalazek/program.cmd
@@ -29,8 +29,8 @@ End_Parallel_Actions
 
 Begin_Parallel_Actions
 Move Car 0.2  25
-Rotate Car.PP 5 Y 1000 
-Rotate Car.LP 5 Y 1000 
-Rotate Car.PT 5 Y 1000 
-Rotate Car.LT 5 Y 1000 
+Rotate Car.PP 5 Y 700 
+Rotate Car.LP 5 Y 700 
+Rotate Car.PT 5 Y 700
+Rotate Car.LT 5 Y 700 
 End_Parallel_Actions  
